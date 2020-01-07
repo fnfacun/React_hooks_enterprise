fe95970fc84802518704f1853e7e5570b2929d72
fn <3456104398@qq.com>
2020-01-07 10:55:05 +0800

    add butter-scroll

--- src/common/component/tab.js
+++ src/common/component/tab.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import BScroll from 'better-scroll';
 import "../css/index.css";
 
 function Tab(props) {
     let { data, render } = props;
+    let bScroll = null;
+    let bannerWrap = useRef(null);
+    let [now, setNow] = useState(0);
+    useEffect(() => {
+        let timer = null;
+        bScroll = new BScroll(bannerWrap.current, {
+            scrollX: true,
+            scrollY: false,
+            eventPassthrough: "vertical",
+            momentum: false,  // 单张切换
+            snap: {
+                loop: true  // 无缝滚动
+            }
+        });
+        // 同步 active
+        bScroll.on("scrollEnd",()=>{
+            console.log("1")
+            setNow(bScroll.getCurrentPage().pageX)
+        });
+        // 开启定时器
+        timer = setInterval(() => {
+            bScroll.next(600)
+        }, 3000);
+        // 按下清除
+        bannerWrap.current.addEventListener("touchstart",()=>{
+            clearInterval(timer);
+        });
+        // 抬起执行
+        bannerWrap.current.addEventListener("touchend",()=>{
+            timer = setInterval(() => {
+                bScroll.next(600)
+            }, 3000);
+        });
+        return ()=>{
+            clearInterval(timer);
+        }
+    }, []);
     return (
         <div className="banner">
-            <div className="banner_img">
+            <div className="banner_img" ref={bannerWrap}>
                 <ul className="banner_list clearfix">
-                {
-                    data.map((item,index)=><li key={index}>{render(item)}</li>)
-                }
+                    {
+                        data.map((item, index) => <li key={index}>{render(item)}</li>)
+                    }
                 </ul>
             </div>
             <ul className="banner_nav">
                 {
-                    data.map((item,index)=><li key={index}></li>)
+                    data.map((item, index) => <li key={index} className={now==index?"active":""} ></li>)
                 }
             </ul>
         </div>
